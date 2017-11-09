0caf3371e64b52c4c55e5ff4304df573c52c2a41
JhamniYoungShinnick <jyou1904@mpsedu.org>
2017-11-09 09:03:45 -0600

    Add files via upload

--- moris_code.ino
+++ moris_code.ino
@@ -10,12 +10,26 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   dot();
+  dash();
+  space();
 }
 //*dot
 void dot(){
    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a .1second
+  delay(200);                       // wait for a .1second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(unit);                       // wait for a .8second
+}
+void dash(){
+   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(600);                       // wait for a .1second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(unit);                       // wait for a .8second
 }
 
+void space(){
+   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(0);                       // wait for a .1second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(unit);                       // wait for a .8second
+}
